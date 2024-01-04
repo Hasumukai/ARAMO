bf7cce5bd393e4cba658cf55eade16db6f6b9490
K. Fukuda <134115260+KuFukuda@users.noreply.github.com>
2024-01-04 16:53:15 +0900

    Add files via upload

--- AutoMeteorObervetion_RealTime_Remote_v2.sh
+++ AutoMeteorObervetion_RealTime_Remote_v2.sh
@@ -3,9 +3,15 @@
 freq_base=$1
 cal_sym=$2
 freq_diff=$3
-/home/dietpi/workspace/meteor/WaveMeasure/AutoMeteorObervetion_rtl-sdr_v3.sh $freq_base $cal_sym $freq_diff &
 
-/home/dietpi/workspace/meteor/WaveMeasure/AutoMeteorObervetion_RealTime1_v4.sh $freq_base $cal_sym $freq_diff &
-/home/dietpi/workspace/meteor/WaveMeasure/AutoMeteorObervetion_RealTime2_v4.sh $freq_base $cal_sym $freq_diff
+main_path=/home/dietpi/workspace/meteor/WaveMeasure/
+
+$main_path/AutoMeteorObervetion_rtl-sdr_v3.sh $freq_base $cal_sym $freq_diff &
+$main_path/AutoMeteorObervetion_RealTime1_v5.sh $freq_base $cal_sym $freq_diff $main_path&
+$main_path/AutoMeteorObervetion_RealTime2_v5.sh $freq_base $cal_sym $freq_diff $main_path
+
+#/home/dietpi/workspace/meteor/WaveMeasure/AutoMeteorObervetion_rtl-sdr_v3.sh $freq_base $cal_sym $freq_diff &
+#/home/dietpi/workspace/meteor/WaveMeasure/AutoMeteorObervetion_RealTime1_v4.sh $freq_base $cal_sym $freq_diff &
+#/home/dietpi/workspace/meteor/WaveMeasure/AutoMeteorObervetion_RealTime2_v4.sh $freq_base $cal_sym $freq_diff
 
 #echo "end"
