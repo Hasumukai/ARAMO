2b177327b58b7fa8afd5eb37f898d34ebadc6a53
Hasumukai <134115260+Hasumukai@users.noreply.github.com>
2026-04-23 00:51:22 +0900

    Add files via upload

--- fftw3_spectrogram_Realtime_v0.4.c
+++ fftw3_spectrogram_Realtime_v0.4.c
@@ -7,11 +7,14 @@
 #include<time.h>
 
 #define MAX 1000000
-#define SRATE 120000 //sampling rate [Hz]
-#define WINDOW_LENGTH 120000
+#define SRATE 48000 //sampling rate [Hz]
+#define WINDOW_LENGTH 48000
+//#define SRATE 120000 //sampling rate [Hz]
+//#define WINDOW_LENGTH 120000
 //#define SHIFT_LENGTH 60000
 //#define SHIFT_LENGTH 24000
-#define SHIFT_LENGTH 120000
+//#define SHIFT_LENGTH 120000
+#define SHIFT_LENGTH 48000
 //#define START_FREQ 700
 //#define END_FREQ 1100
 //#define START_FREQ 0
@@ -26,17 +29,17 @@ int main (int argc,char **argv)
 	int i;
 	short *Y; 
 	int BlockSize=WINDOW_LENGTH*2;
-	unsigned int fpStart;
+//	unsigned int fpStart;
 	int ret;
 	int add;
 	fftw_complex *in, *out;
-	struct timeval time_s,time_e;
-	struct timespec time_sleep;
-	double time_calc;
+//	struct timeval time_s,time_e;
+//	struct timespec time_sleep;
+//	double time_calc;
 	int start_freq,end_freq;
 
 	//time
-	gettimeofday(&time_s,NULL);
+//	gettimeofday(&time_s,NULL);
 
 	fp = fopen(argv[1], "r");
 	if (fp == NULL){
@@ -48,45 +51,60 @@ int main (int argc,char **argv)
 			exit(-1);
 	}
 
-	fpStart=atoi(argv[2]);
-	add=fpStart/SHIFT_LENGTH;
-	fpStart*=2;	//1point = 2byte
+//	fpStart=atoi(argv[2]);
+//	add=fpStart/SHIFT_LENGTH;
+//	fpStart*=2;	//1point = 2byte
 
-	start_freq=atoi(argv[3]);
-	end_freq=atoi(argv[4]);
+//	start_freq=atoi(argv[2]);
+//	end_freq=atoi(argv[3]);
+	start_freq=600;
+	end_freq=1200;
 
-	if(ret=fseek(fp, fpStart, SEEK_SET)){
-			fprintf(stderr, "%d:fseek error.\n",ret);
-			exit(-1);
-   }
-    if( fread(Y, sizeof(short), BlockSize, fp) == 0){
-		fprintf(stderr,"read miss.");
-		exit(-1);
-	}
-	fclose(fp);
+//	if(ret=fseek(fp, fpStart, SEEK_SET)){
+//			fprintf(stderr, "%d:fseek error.\n",ret);
+//			exit(-1);
+//  }
+//	fclose(fp);
 
 	in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * WINDOW_LENGTH);
 	out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * WINDOW_LENGTH);
-	for(i=0;i*SHIFT_LENGTH<BlockSize/2;i++){
+
+	for(i=1;i<602;i++){
+
+  if( fread(Y, sizeof(short), BlockSize, fp) == 0){
+		fprintf(stderr,"read miss.");
+		exit(-1);
+	}
+
+//	for(i=0;i*SHIFT_LENGTH<BlockSize/2;i++){
 //		calc_fft(&Y[i*SHIFT_LENGTH],in,out);
 //		calc_fft(i+add,&Y[i*SHIFT_LENGTH],in,out);
-		calc_fft(&Y[i*SHIFT_LENGTH],in,out,start_freq,end_freq);
+//		calc_fft(&Y[i*SHIFT_LENGTH],in,out,start_freq,end_freq);
+		calc_fft(Y,in,out,start_freq,end_freq);
 		printf("\n");
+//	}
+
+	if(ret=fseek(fp, i*SRATE*2, SEEK_SET)){
+			fprintf(stderr, "%d:fseek error.\n",ret);
+			exit(-1);
+  }
 	}
+	fclose(fp);
+
 	fftw_free(in); 
 	fftw_free(out);
 
 	free(Y);
 
-	gettimeofday(&time_e,NULL);
+//	gettimeofday(&time_e,NULL);
 
-	time_calc=1e9*(time_e.tv_sec-time_s.tv_sec)+1e3*(time_e.tv_usec-time_s.tv_usec);
-	if(time_calc<1e9){
-		time_sleep.tv_sec=0;
-		time_sleep.tv_nsec=1e9-time_calc;
+//	time_calc=1e9*(time_e.tv_sec-time_s.tv_sec)+1e3*(time_e.tv_usec-time_s.tv_usec);
+//	if(time_calc<1e9){
+//		time_sleep.tv_sec=0;
+//		time_sleep.tv_nsec=1e9-time_calc;
 	
-		nanosleep(&time_sleep,NULL);
-	}
+//		nanosleep(&time_sleep,NULL);
+//	}
 
 }
 
@@ -112,8 +130,8 @@ void calc_fft(short *Ys,fftw_complex *in,fftw_complex *out,int start_freq,int en
 		t = out[i][1];		
 		u = sqrt(s*s + t*t)/1e4;
 //		printf("%f \t %d \t %.5lf\n", (float)(BlockNumber)*SHIFT_LENGTH/SRATE,i, u);
-//		printf("%.1lf ",u);
 		printf("%.1lf ",u);
+//		printf("%.1lf\n",u);
 	}
 
 	fftw_destroy_plan(p);
